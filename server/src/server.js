c79547638836a862e4603cfcdf1a7749da29314a
AbhishekGour12 <aggour124421@gmail.com>
2026-05-29 11:30:50 +0530

    first commit

--- server/src/server.js
+++ server/src/server.js
@@ -25,7 +25,27 @@ import http from "http";
 
 const app = express();
 dotenv.config();
-app.use(cors());
+
+const allowedOrigins = [
+  "https://www.guttalks.in",
+  "https://guttalks.in",
+  "http://localhost:3000" // Local testing ke liye
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 MongoDBConnect();
